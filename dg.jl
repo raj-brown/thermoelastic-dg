20edde128e6c9b0bfe38f185edab24e8462d3fd7
raj-brown <khemraj_shukla@brown.edu>
2026-03-22 19:42:37 -0400

    Working on RHS

--- dg.jl
+++ dg.jl
@@ -1,26 +1,29 @@
 using StaticArrays
 
 
-function rhs!(dU, u, parameters, time)
+function rhs!(du_cache, u, parameters, time)
     Np, K = parameters.rd.Np, parameters.md.K
     N = Np*K
+
+    (; rd, md) = parameters
+    (; Vf, Dr, Ds, LIFT) = rd
+    (; rxJ, sxJ, ryJ, syJ, nxJ, nyJ, nx, ny, J, Jf, mapP) = md
+       
+
+
     s11 = reshape(@view(u[1:N]), Np, K)
     s12 = reshape(@view(u[N+1:2N]), Np, K)
     s22 = reshape(@view(u[2N+1:3N]), Np, K)
     v1 = reshape(@view(u[3N+1:4N]), Np, K)
     v2 = reshape(@view(u[4N+1:5N]), Np, K)
-   
-    ds11 = reshape(@view(dU[1:N]), Np, K)
-    ds12 = reshape(@view(dU[N+1:2N]), Np, K)
-    ds22 = reshape(@view(dU[2N+1:3N]), Np, K)
-    dv1 = reshape(@view(dU[3N+1:4N]), Np, K)
-    dv2 = reshape(@view(dU[4N+1:5N]), Np, K)
 
-    ds11 .= 0.0
-    ds12 .= 0.0
-    ds22 .= 0.0
-    dv1 .= 0.0
-    dv2 .= 0.0
+    uM = Vf * s11
+    uP = uM[mapP]
+
+
+   
+   
+   
     nothing 
 end
 
--- main.jl
+++ main.jl
@@ -10,6 +10,14 @@ using OrdinaryDiffEq
 include("dg.jl")
 
 println("running simulation")
+# Set up the problem
+μ = 1.0
+λ = 1.0
+c11 = λ + 2μ
+c12 = λ
+c22 = λ + 2μ
+
+
 N=3
 K1D = 8
 
@@ -25,4 +33,5 @@ rhs!(du, u, params, 0.0)
 tspan = (0.0, 3.0)
 ode = ODEProblem(rhs!, u, tspan, params)
 
-sol = solve(ode, Tsit5())
+sol = solve(ode, Tsit5(), saveat=LinRange(tspan..., 10), 
+            abstol=1e-10, reltol=1e-10)
