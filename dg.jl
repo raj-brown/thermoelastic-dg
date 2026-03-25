7c87f5de6ae6255e6815d358ea9076d8b315dbe7
raj-brown <khemraj_shukla@brown.edu>
2026-03-24 21:14:06 -0400

    Added Elastic 2D

--- dg.jl
+++ dg.jl
@@ -1,9 +1,18 @@
 using StaticArrays
 
 
-function rhs!(du_cache, u, parameters, time)
+function rhs!(du, u, parameters, time)
+
+
+    μ = 1.0
+    λ = 2.0
+    c11 = λ + 2μ
+    c12 = λ
+    c33 = μ
+    c22 = c11
+    rho = 1.0
+
     Np, K = parameters.rd.Np, parameters.md.K
-    N = Np*K
 
     (; rd, md) = parameters
     (; Vf, Dr, Ds, LIFT) = rd
@@ -11,18 +20,116 @@ function rhs!(du_cache, u, parameters, time)
        
 
 
-    s11 = reshape(@view(u[1:N]), Np, K)
-    s12 = reshape(@view(u[N+1:2N]), Np, K)
-    s22 = reshape(@view(u[2N+1:3N]), Np, K)
-    v1 = reshape(@view(u[3N+1:4N]), Np, K)
-    v2 = reshape(@view(u[4N+1:5N]), Np, K)
+    s11 = view(u, :, :, 1)
+    s22 = view(u, :, :, 2)
+    s12 = view(u, :, :, 3)
+    v1 = view(u, :, :, 4)
+    v2 = view(u, :, :, 5)
+    
+    s11r = Dr * s11 
+    s11s = Ds * s11
+    
+    s11x = rxJ .* s11r + sxJ .* s11s
+    s11y = ryJ .* s11r + syJ .* s11s
+    
+    
+    s12r = Dr * s12 
+    s12s = Ds * s12
+    
+    s12x = rxJ .* s12r + sxJ .* s12s
+    s12y = ryJ .* s12r + syJ .* s12s
 
-    uM = Vf * s11
-    uP = uM[mapP]
+
+    s22r = Dr * s22 
+    s22s = Ds * s22
+    s22x = rxJ .* s22r + sxJ .* s22s
+    s22y = ryJ .* s22r + syJ .* s22s
+
+    v1r = Dr * v1
+    v1s = Ds * v1
+    v1x = rxJ .* v1r + sxJ .* v1s
+    v1y = ryJ .* v1r + syJ .* v1s
+
+    v2r = Dr * v2
+    v2s = Ds * v2
+    v2x = rxJ .* v2r + sxJ .* v2s
+    v2y = ryJ .* v2r + syJ .* v2s
 
 
-   
-   
+    s11m = Vf * s11
+    s11p = s11m[mapP]
+    ds11 = s11p - s11m
+    
+    s12m = Vf * s12
+    s12p = s12m[mapP]
+    ds12 = s12p - s12m
+
+    s22m = Vf * s22
+    s22p = s22m[mapP]
+    ds22 = s22p - s22m
+
+    v1m = Vf * v1
+    v1p = v1m[mapP]
+    dv1 = v1p - v1m
+
+    v2m = Vf * v2
+    v2p = v2m[mapP]
+    dv2 = v2p - v2m
+
+    divsx = s11x + s12y
+    divsy = s12x + s22y
+    vxy   = v1y + v2x
+
+
+    # Compute the velocity fluxes
+    nSx = nx .* ds11 + ny.* ds12
+    nSy = nx .* ds12 + ny.* ds22
+
+
+    # Impose BC's
+
+
+    # Stress fluxes
+    nv11x = dv1 .* nx
+    nv11y = dv2 .* ny
+    nvxy = dv2.* nx + dv1 .* ny
+
+    fcs11 = nv11x
+    fcs22 = nv11y
+    fcs12 = nvxy
+
+    fcv1 = nSx
+    fcv2 = nSy
+
+    # Penalization term
+
+    fpenalty_s11 = fcv1.*nxJ
+    fpenalty_s22 = fcv2.*nyJ
+    fpenalty_s12 = fcv2.*nxJ + fcv1.*nyJ
+
+    fpenalty_v1 = nxJ.*fcs11 + nyJ.*fcs12
+    fpenalty_v2 = nxJ.*fcs12 + nyJ.*fcs22
+    
+    tau = 1.0
+    fluxS11 = @. fcs11 + tau * fpenalty_s11 
+    fluxS22 = @. fcs22 + tau * fpenalty_s22
+    fluxS12 = @. fcs12 + tau * fpenalty_s12 
+    fluxv1 = @. fcv1 + tau * fpenalty_v1
+    fluxv2 = @. fcv2 + tau * fpenalty_v2
+
+
+    du[:, :, 1] .= (v1x + 0.5*LIFT *(fluxS11 .* Jf))./J
+    du[:, :, 2] .= (v1y + 0.5*LIFT *(fluxS22 .* Jf))./J
+    du[:, :, 3] .= (vxy + 0.5*LIFT *(fluxS12 .* Jf))./J
+    du[:, :, 4] .= (divsx + 0.5*LIFT *(fluxv1 .* Jf))./J
+    du[:, :, 5] .= (divsy + 0.5* LIFT *(fluxv2 .* Jf))./J
+
+    du[:, :, 1] = c11.* du[:, :, 1] + c12.* du[:, :, 2]
+    du[:, :, 2] = c12.* du[:, :, 1] + c22.* du[:, :, 2] 
+    du[:, :, 3] = c33.* du[:, :, 3]
+
+    du[:, :, 4] .= du[:, :, 4]./rho
+    du[:, :, 5] .= du[:, :, 5]./rho
    
     nothing 
 end
@@ -32,12 +139,5 @@ function point_force(t)
     println("Adding point force at time $t")
 end
 
-function initial_condition(Np, K)
-    s11 = zeros(Np, K)
-    s12 = zeros(Np, K)
-    s22 = zeros(Np, K)
-    v1 = zeros(Np, K)
-    v2 = zeros(Np, K)
-    vcat(vec(s11), vec(s12), vec(s22), vec(v1), vec(v2))
-end
+
 
--- main.jl
+++ main.jl
@@ -6,32 +6,44 @@ using StartUpDG
 using OrdinaryDiffEq
 using LinearAlgebra
 using OrdinaryDiffEq
+using Plots
 
 include("dg.jl")
 
 println("running simulation")
 # Set up the problem
-μ = 1.0
-λ = 1.0
-c11 = λ + 2μ
-c12 = λ
-c22 = λ + 2μ
 
+gaussian(x, y, s) = exp(-(x^2 + y^2) / (2*s*s))
 
-N=3
+N=2
 K1D = 8
 
 rd = RefElemData(Tri(), N)
 VXY, EToV = uniform_mesh(Tri(), K1D)
 md = MeshData(VXY, EToV, rd)
+md = make_periodic(md)
+
 
 (; x, y) = md
-u = initial_condition(rd.Np, md.K)
+u = zeros(rd.Np, md.K, 5)
+u[:, :, 5] = gaussian.(x, y, 0.01) 
+
+xp, yp = vec(rd.Vp * x), vec(rd.Vp * y)  
+
+scatter(xp, yp, zcolor=vec(rd.Vp*u[:,:,5]), markersize=2, markerstrokewidth=0, legend=false, clims=(-.25, .25))
+
+
 params = (; rd, md)
 du = similar(u)
 rhs!(du, u, params, 0.0)
-tspan = (0.0, 3.0)
+tspan = (0.0, 1.0)
 ode = ODEProblem(rhs!, u, tspan, params)
 
 sol = solve(ode, Tsit5(), saveat=LinRange(tspan..., 10), 
-            abstol=1e-10, reltol=1e-10)
+            abstol=1e-14, reltol=1e-7)
+
+xp, yp = vec(rd.Vp * x), vec(rd.Vp * y)   
+@gif for i in eachindex(sol.u)    
+    scatter(xp, yp, zcolor=vec(rd.Vp * sol.u[i][:,:,5]), 
+            markersize=2, markerstrokewidth=0, legend=false, clims=(-.25, .25))
+end
